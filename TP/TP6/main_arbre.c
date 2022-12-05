e16c1600e9e5569fe3f714ad0ad0388f5a899f2a
Iron68A <84898382+Iron68A@users.noreply.github.com>
2022-12-05 18:06:16 +0100

    Update main_arbre.c

--- TP/TP6/main_arbre.c
+++ TP/TP6/main_arbre.c
@@ -13,10 +13,7 @@ int main(){
     Arbre *a = join_arbre(cree_arbre(2),cree_arbre(1),4);
     Arbre *b = join_arbre(cree_arbre(0),NULL,3);
     Arbre *c = join_arbre(b,a,5);
-
     affiche_arbre_prefixe(c);
     vider_arbre(c);
-
-    
     return 0;
 }
